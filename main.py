ddeb721384a9b7618737b2d1cd88a436c6c9b247
Karina Chernobai <karinachernobay@gmail.com>
2023-05-13 13:00:49 +0300

    Task3 is complete

---
+++ main.py
@@ -0,0 +1,11 @@
+from task3 import task3
+from task5 import task5
+from task6 import task6
+from task7 import task7
+from task9 import task9
+
+task3()
+# task5()
+# task6()
+# task7()
+# task9()
---
+++ task9.py
@@ -0,0 +1,2 @@
+def task9():
+    return
